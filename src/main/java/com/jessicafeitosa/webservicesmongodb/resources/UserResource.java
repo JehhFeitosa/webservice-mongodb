d6a515703a34d7e82ff97b426026a0debd5e5f3e
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-12-18 22:26:18 -0300

    implements pattern DTO for retorn users

--- src/main/java/com/jessicafeitosa/webservicesmongodb/resources/UserResource.java
+++ src/main/java/com/jessicafeitosa/webservicesmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.jessicafeitosa.webservicesmongodb.resources;
 
 import com.jessicafeitosa.webservicesmongodb.domain.User;
+import com.jessicafeitosa.webservicesmongodb.dto.UserDTO;
 import com.jessicafeitosa.webservicesmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService service;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 
 }
